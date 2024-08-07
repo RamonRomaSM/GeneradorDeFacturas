c6cda859350389a168b8e5dca0aa29a86d323e31
RamonRomaSM <125303729+RamonRomaSM@users.noreply.github.com>
2024-08-07 19:49:22 +0200

    a

---
+++ .vs/BillGenerator/diccionarios/Dictionary.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+
+namespace BillGenerator.diccionarios
+{
+    public partial class Dictionary : ResourceDictionary
+    {
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = (Button)sender;
+            b.Background = (Brush)new BrushConverter().ConvertFrom("#778DA9");
+        }
+        private void hover(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            b.Background = (Brush)new BrushConverter().ConvertFrom("#778DA9");
+        }
+    }
+   
+}
